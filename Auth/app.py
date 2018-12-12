f8c728847562a3b90584e1581a2ca0a4a144bd93
Julio45andres <julian.munozm@udea.edu.co>
2018-12-11 22:08:56 -0500

    genera tokens

--- Auth/app.py
+++ Auth/app.py
@@ -1,63 +1,58 @@
 import pyrebase
+import firebase_admin
+from firebase_admin import credentials, auth
 from flask import *
+import os
 app = Flask(__name__)
-config = {
-    "apiKey": "",
-    "authDomain": "",
-    "databaseURL": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": ""
-}
+# config = {
+#     "apiKey": "AIzaSyBYHpRerd0Dit9PnuF8e4JcxSj34fZqPbw",
+#     "authDomain": "renty-vue.firebaseapp.com",
+#     "databaseURL": "https://renty-vue.firebaseio.com",
+#     "projectId": "renty-vue",
+#     "storageBucket": "renty-vue.appspot.com",
+#     "messagingSenderId": "28025495187"
+# }
 
-firebase = pyrebase.initialize_app(config)
+# firebase = pyrebase.initialize_app(config)
 
-auth = firebase.auth()
+# auth = firebase.auth()
+
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+cred = credentials.Certificate(os.path.join(
+    my_path, "../renty-python-firebase-adminsdk.json"))
+default_app = firebase_admin.initialize_app(cred)
+
+# options = {
+#     'serviceAccountId': 'firebase-adminsdk-rma72@renty-vue.iam.gserviceaccount.com'
+# }
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/julio/dev/FirebasePython/renty-python-firebase-adminsdk.json"
+# firebase_admin.initialize_app(options=options)
+# firebase = pyrebase.initialize_app(config)
+
+# auth = firebase.auth()
+uid = 'LDFi5ZgeRagBTa3AiGMfJlIxZgd2'
+custom_token = auth.create_custom_token(uid)
+print(custom_token)
+
 
 @app.route('/', methods=['GET', 'POST'])
-
 def basic():
-	unsuccessful = 'Please check your credentials'
-	successful = 'Login successful'
-	if request.method == 'POST':
-		email = request.form['name']
-		password = request.form['pass']
-		try:
-			auth.sign_in_with_email_and_password(email, password)
-			return render_template('new.html', s=successful)
-		except:
-			return render_template('new.html', us=unsuccessful)
+    unsuccessful = 'Login unsuccessful'
+    successful = 'Login successful'
+    if request.method == 'POST':
+        email = request.form['name']
+        password = request.form['pass']
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            # id_token = auth.get_account_info(user['idToken'])
+            # print(auth.verify_id_token(id_token)["uid"])
+            return render_template('new.html', s=successful)
+        except:
+            return render_template('new.html', us=unsuccessful)
 
-	return render_template('new.html')
+    return render_template('new.html')
 
 
 if __name__ == '__main__':
-	app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.run()
